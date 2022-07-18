cac55c2fa0331359b31d4f2efb6b33529bc2ff90
Jisu Hwang <ghkdwltn5@naver.com>
2022-07-18 12:03:08 +0900

    MyPrintx

---
+++ src/printTest/PrintTest2.java
@@ -0,0 +1,18 @@
+package printTest;
+
+public class PrintTest2 {
+	public static void main(String[] args) {
+// 제어문자
+// 반드시 따옴표 안에서 작성한다.
+
+// \n	: new line, 줄바꿈, 개행문자
+// \t	: tab, 위 아래 줄 간격 맞처 띄우기
+// \"	: " 표현
+// \'	: ' 표현
+// \\	: \표현
+
+//		System.out.print("황지수\n");
+		System.out.println("황지수");
+		System.out.print("\"20살\"");
+	}
+}
